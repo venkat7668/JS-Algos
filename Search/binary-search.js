7277a19ba5d2c31a1b303b5b4784b1f42523755c
Venkat Lankalapalli <venkateswararao.lankalapalli@sap.com>
2019-06-10 12:03:18 +0530

    Binary search modified

--- Search/binary-search.js
+++ Search/binary-search.js
@@ -1,7 +1,15 @@
+/**
+ * @author Venkat Lankalapalli
+ * @email venkat7668[@]gmail.com
+ * @create date 2019-06-10 11:47:07
+ * @modify date 2019-06-10 11:47:07
+ * @desc find x index in given sorted array
+ */
+
 exports.binarySearch = function (arr, x) {
     let l = 0;
-    let r = arr.length;
-    while (l < r) {
+    let r = arr.length - 1;
+    while (r >= l) {
         let mid = Math.floor((l + r) / 2);
         if (arr[mid] == x) {
             return mid;
