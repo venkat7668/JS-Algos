9364bca9aaf7b7b49ae883284b7bf03cd7d6257a
Venkat Lankalapalli <v.lankalapalli@f5.com>
2022-04-30 13:30:45 +0530

    fix: array and linkedlist alogs added

---
+++ Arrays/sherlock-array.js
@@ -0,0 +1,31 @@
+/*
+ * Problem: find an element of the array such that the sum of all elements
+ *          to the left is equal to the sum of all elements to the right.
+ *
+ * Solution: 
+ * 
+ * arr = [5,6,8,11]
+ * 8 is between two subarrays that sum to 11
+ * 
+ * ____   ____             __            __     .__   
+ * \   \ /   /____   ____ |  | _______ _/  |_   |  |  
+ *  \   Y   // __ \ /    \|  |/ /\__  \\   __\  |  |  
+ *   \     /\  ___/|   |  \    <  / __ \|  |    |  |__
+ *    \___/  \___  >___|  /__|_ \(____  /__| /\ |____/
+ *               \/     \/     \/     \/     \/       
+ */
+
+function findElementBetweenEqualSumElements(arr) {
+    const s = sum(arr);
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (2 * count == s - arr[i]) {
+            return arr[i]
+        }
+        count += arr[i]
+    }
+}
+
+function sum(arr) {
+    return arr.reduce((i, n) => n + i, 0)
+}
---
+++ Singly Linked List/find-intersection-node.js
@@ -0,0 +1,56 @@
+/* 
+ * Problem : Find the intersection node of Y shaped linked list
+ * Solution: 
+ *  #1 Find two linked lists lengths separately 
+ *  #2 Find the diffrence between 2 lengths 
+ *  #3 Find the diff nth node of the longer linked list
+ *  #4 Now you have same length linked lists to verify
+ *  #5 Gothrough the linked list nodes and find the common node
+ *  #6 And return the node data/value
+ *
+ * time complexity: O(M+N)
+ * 
+ * ____   ____             __            __     .__   
+ * \   \ /   /____   ____ |  | _______ _/  |_   |  |  
+ *  \   Y   // __ \ /    \|  |/ /\__  \\   __\  |  |  
+ *   \     /\  ___/|   |  \    <  / __ \|  |    |  |__
+ *    \___/  \___  >___|  /__|_ \(____  /__| /\ |____/
+ *               \/     \/     \/     \/     \/       
+ */
+
+function findIntersectionNode (head1, head2){
+    const n1Len = getLength(head1);
+    const n2Len = getLength(head2);
+    let longList, shortList, diff, intersectionNode = null;
+
+    if(n1Len > n2Len){
+        longList = head1;
+        shortList = head2;
+        diff = n1Len - n2Len;
+    }else {
+        longList = head2;
+        shortList = head1;
+        diff = n2Len - n1Len;
+    }
+
+    while(diff > 0){
+        longList = longList.next;
+        diff--;
+    }
+
+    while(longList.next && shortList.next){
+        if(longList.next == shortList.next){
+            intersectionNode = longList.next;
+            return
+        }
+    }
+    return intersectionNode
+}
+
+function getLength(node){
+    let count = 0;
+    while(node.next){
+        count++
+    }
+    return count
+}
